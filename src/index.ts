770fa1834d62dcc646d1cb187abc72514c8a500c
Alex T <aletapia@proton.me>
2025-04-01 16:49:23 -0600

    (feat): enhance Job model in Prisma schema with additional fields and update .gitignore to match new patterns.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const job = await prisma.job.create({
-  data: { title: "Software Engineer" },
-});
-
-console.log(job);
+console.log("Hello via Bun!");
