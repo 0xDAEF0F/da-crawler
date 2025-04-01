40c24e95800995ec15483c8d3347b08e09923fac
Alex T <aletapia@proton.me>
2025-04-01 16:35:44 -0600

    (feat): add Prisma integration for job creation and update package scripts for building output.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const job = await prisma.job.create({
+  data: { title: "Software Engineer" },
+});
+
+console.log(job);
