e9a460d2a05ae5c064f489cfa311f9391d3bdb6f
Alex T <aletapia@proton.me>
2025-04-13 12:40:13 -0600

    (feat): add root endpoint to return a greeting and log connection information in index.ts.

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,23 @@
 import { Hono } from "hono";
 import { getTags } from "./routes/all-tags";
 import { getJobs } from "./routes/get-jobs";
+import { getConnInfo } from "hono/bun";
 
-console.log(`Server starting at ${new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}`);
+console.log(
+  `Server starting at ${new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  })}`
+);
 
 const app = new Hono();
 
 app.post("/get-jobs", getJobs);
 app.get("/all-tags", getTags);
+app.get("/", (c) => {
+  console.log(`Request from: ${getConnInfo(c).remote.address}`);
+  return c.text("Hello World");
+});
 
 export default app;
