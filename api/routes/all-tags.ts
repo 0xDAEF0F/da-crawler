4648f395472eda6aa3d8f130ae22c6cf4f0d0f39
Alex T <aletapia@proton.me>
2025-12-09 15:15:21 -0700

    fixed tags

--- api/routes/all-tags.ts
+++ api/routes/all-tags.ts
@@ -4,47 +4,58 @@ import { uniq } from "lodash";
 import { type } from "arktype";
 
 export async function getTags(c: Context) {
-  const tags = await prisma.job.findMany({
-    select: {
-      keywords: true,
-    },
-  });
-
-  // Flatten all tags and count their frequency
-  const tagFrequencyMap = new Map<string, number>();
-
-  tags.forEach((t) => {
-    const combinedTags = [
-      ...JSON.parse(t.keywords),
-      // ...(JSON.parse(t.aiAnalysis?.keywords ?? "[]") as string[]),
-    ];
-    combinedTags.forEach((tag) => {
-      // const tag_ = tag.toLowerCase().trim();
-      tagFrequencyMap.set(tag, (tagFrequencyMap.get(tag) || 0) + 1);
+  try {
+    const tags = await prisma.job.findMany({
+      select: {
+        keywords: true,
+      },
     });
-  });
 
-  // Sort tags by frequency in descending order
-  const tags_ = uniq(
-    Array.from(tagFrequencyMap.entries())
-      .sort((a, b) => b[1] - a[1])
-      .map(([tag]) => tag),
-  );
+    // Flatten all tags and count their frequency
+    const tagFrequencyMap = new Map<string, number>();
 
-  const validated = type("string[]")(tags_);
+    tags.forEach((t) => {
+      const combinedTags = [
+        ...JSON.parse(t.keywords),
+        // ...(JSON.parse(t.aiAnalysis?.keywords ?? "[]") as string[]),
+      ];
+      combinedTags.forEach((tag) => {
+        // const tag_ = tag.toLowerCase().trim();
+        tagFrequencyMap.set(tag, (tagFrequencyMap.get(tag) || 0) + 1);
+      });
+    });
 
-  // TODO: Abstract this a bit more
-  if (validated instanceof type.errors) {
+    // Sort tags by frequency in descending order
+    const tags_ = uniq(
+      Array.from(tagFrequencyMap.entries())
+        .sort((a, b) => b[1] - a[1])
+        .map(([tag]) => tag),
+    );
+
+    const validated = type("string[]")(tags_);
+
+    // TODO: Abstract this a bit more
+    if (validated instanceof type.errors) {
+      return c.json(
+        {
+          error: true,
+          message: "Internal server error",
+        },
+        { status: 500 },
+      );
+    }
+    return c.json({
+      error: false,
+      tags: validated,
+    });
+  } catch (e) {
+    console.error("Error in getTags:", e);
     return c.json(
       {
         error: true,
-        message: "Internal server error",
+        message: String(e),
       },
       { status: 500 },
     );
   }
-  return c.json({
-    error: false,
-    tags: validated,
-  });
 }
